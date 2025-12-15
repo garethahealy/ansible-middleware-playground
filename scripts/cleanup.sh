c42f8f0058c676f5b5d852913837c1622924a3de
Gareth Healy <garethahealy@gmail.com>
2025-12-15 13:03:20 +0000

    next key
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -5,3 +5,4 @@ rm -f configure-aws/roles/configure_bootstrap/files/*.pem
 rm -f configure-aws/roles/configure_bootstrap/files/*.pem.enc
 rm -f configure-aws/roles/configure_bootstrap/files/*.pub
 rm -f configure-aws/inventory/*.yml
+rm -f configure-aws/vault-password.sec
